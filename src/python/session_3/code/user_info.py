58ece5d69b0274c5b4ed281d799689e690b806db
Abdelrahman-Magd <abdalrahmanmagd03@gmail.com>
2025-12-13 21:51:10 +0200

    updated session 3

---
+++ src/python/session_3/code/user_info.py
@@ -0,0 +1,13 @@
+def info_user(name,age):
+    """  
+    param_1 : take y= the name from user
+    type param_1 : str
+
+    param_2 : take age from user
+    type param_2 : int
+
+    return : return a string of the name and the age
+    type return : str
+
+    """
+    return f"my name is {name} and my age is {age} years old"
